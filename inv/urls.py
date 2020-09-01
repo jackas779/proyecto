cc64363af1df7f1423c48c9f3d4f59346451aa27
jackas <jackas779@gmail.com>
2020-09-01 16:55:38 -0500

    Se agrego boton de inactivar/activar en vista categoria

--- inv/urls.py
+++ inv/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
-from .views import CategoriaList, CategoriaEdit
+from .views import CategoriaList, CategoriaEdit, categoria_inactivar
 
 urlpatterns = [
     path('categorias/', CategoriaList.as_view(), name="categoria_list"),
     path('categorias/edit/<int:pk>', CategoriaEdit.as_view(), name='categoria_edit'),
+    path('categorias/inactivar/<int:id>', categoria_inactivar, name='categoria_inactivar'),
 ]
--- inv/views.py
+++ inv/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView, ListView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 #Django bootstrap model
 from bootstrap_modal_forms.generic import BSModalUpdateView
@@ -13,16 +14,33 @@ from .forms import CategoriaForm
 from .models import Categorias
 
 
-class CategoriaList(ListView):
+class CategoriaList(LoginRequiredMixin, ListView):
     """Vista para mostrar todas las categorías"""
     model = Categorias
     template_name = 'inv/categorias.html'
     context_object_name = 'categorias'
 
 
-class CategoriaEdit(BSModalUpdateView):
+class CategoriaEdit(LoginRequiredMixin, BSModalUpdateView):
     """Vista para editar categorías"""
     model = Categorias
     template_name = 'inv/categoriaForm.html'
     form_class = CategoriaForm
     success_url = reverse_lazy('categoria_list')
+
+    def form_valid(self, form):
+        form.instance.usuario_modificacion = self.request.user.id
+        return super().form_valid(form)
+
+
+def categoria_inactivar(request, id):
+    model = Categorias.objects.get(pk=id)
+    
+    if request.method == "GET":
+        if model.estado is True:
+            model.estado = False
+        else:
+            model.estado = True
+        model.save()
+    
+    return redirect("categoria_list")
